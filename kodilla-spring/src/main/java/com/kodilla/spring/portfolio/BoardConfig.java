a76c94a0c23a4ac8bb17a0857371098d331b21ff
Michal Kudyk <kudykmichal@gmail.com>
2022-01-04 09:43:34 +0100

    14.3.2

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,11 +1,25 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
+
+    @Autowired
+    @Qualifier("To Do List")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("In Progress List")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("Done List")
+    TaskList doneList;
 
     @Bean(name="To Do List")
     @Scope("singleton")
@@ -28,7 +42,7 @@ public class BoardConfig {
     @Bean
     @Scope("singleton")
     public Board getBoard() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
 
 }
